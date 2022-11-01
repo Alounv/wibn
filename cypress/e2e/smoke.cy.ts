23863acc71312f41435d5857efd6b77c688fa571
Aloun <alounv@gmail.com>
2022-11-02 00:33:59 +0100

    Make CI work

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -40,11 +40,15 @@ describe("smoke tests", () => {
 
     cy.findByRole("link", { name: /\+ new note/i }).click();
 
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
+    cy.findByRole("textbox", { name: /title/i }).type(testNote.title, {
+      force: true,
+    });
+    cy.findByRole("textbox", { name: /body/i }).type(testNote.body, {
+      force: true,
+    });
+    cy.findByRole("button", { name: /save/i }).click({ force: true });
 
-    cy.findByRole("button", { name: /delete/i }).click();
+    cy.findByRole("button", { name: /delete/i }).click({ force: true });
 
     cy.findByText("No notes yet");
   });
