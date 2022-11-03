aa80ed5376695d0f0625d346fe939933c4001a86
Aloun <alounv@gmail.com>
2022-11-03 22:11:11 +0100

    WIP

--- app/routes/groups/$groupId.tsx
+++ app/routes/groups/$groupId.tsx
@@ -1,7 +1,10 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useCatch, useLoaderData } from "@remix-run/react";
+import { Form, useActionData, useCatch, useLoaderData } from "@remix-run/react";
+import { useRef, useState } from "react";
 import invariant from "tiny-invariant";
+import { Button } from "~/components/Button";
+import { LabelledInput } from "~/components/LabelledInput";
 
 import { deleteGroup, getGroup } from "~/models/group.server";
 import { requireUserId } from "~/session.server";
@@ -28,12 +31,48 @@ export async function action({ request, params }: ActionArgs) {
 
 export default function GroupDetailsPage() {
   const data = useLoaderData<typeof loader>();
+  const [isOnEditMode, setIsOnEditMode] = useState(false);
+  const actionData = useActionData<typeof action>();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <div>
-      <h3 className="text-2xl font-bold">{data.group.name}</h3>
-      <p className="py-6">{data.group.description}</p>
-      <hr className="my-4" />
+      {!isOnEditMode ? (
+        <>
+          <h3 className="text-2xl font-bold">{data.group.name}</h3>
+          <p className="py-6">{data.group.description}</p>
+          <hr className="my-4" />
+        </>
+      ) : (
+        <Form method="patch">
+          <LabelledInput
+            label="Name"
+            ref={nameRef}
+            name="name"
+            error={actionData?.errors?.name}
+          />
+
+          <LabelledInput
+            rows={4}
+            label="Description"
+            ref={descriptionRef}
+            name="description"
+            error={actionData?.errors?.description}
+          />
+
+          <div className="text-right">
+            <Button type="submit">Save</Button>
+          </div>
+        </Form>
+      )}
+
+      <button
+        className="rounded border-2 border-blue-500 py-2 px-4 text-blue-500 hover:bg-blue-100 focus:bg-blue-400"
+        onClick={() => setIsOnEditMode(true)}
+      >
+        Edit
+      </button>
       <Form method="post">
         <button
           type="submit"
--- app/routes/groups/new.tsx
+++ app/routes/groups/new.tsx
@@ -2,6 +2,8 @@ import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
 import * as React from "react";
+import { Button } from "~/components/Button";
+import { LabelledInput } from "~/components/LabelledInput";
 
 import { createGroup } from "~/models/group.server";
 import { requireUserId } from "~/session.server";
@@ -22,7 +24,7 @@ export async function action({ request }: ActionArgs) {
 
   if (typeof description !== "string") {
     return json(
-      { errors: { name: null, description: "Description is required" } },
+      { errors: { name: null, description: "Description should be a string" } },
       { status: 400 }
     );
   }
@@ -55,54 +57,23 @@ export default function NewGroupPage() {
         width: "100%",
       }}
     >
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Name: </span>
-          <input
-            ref={nameRef}
-            name="name"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-            aria-invalid={actionData?.errors?.name ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.name ? "name-error" : undefined
-            }
-          />
-        </label>
-        {actionData?.errors?.name && (
-          <div className="pt-1 text-red-700" id="name-error">
-            {actionData.errors.name}
-          </div>
-        )}
-      </div>
+      <LabelledInput
+        label="Name"
+        ref={nameRef}
+        name="name"
+        error={actionData?.errors?.name}
+      />
 
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Description: </span>
-          <textarea
-            ref={descriptionRef}
-            name="description"
-            rows={8}
-            className="w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-3"
-            aria-invalid={actionData?.errors?.description ? true : undefined}
-            aria-errormessage={
-              actionData?.errors?.description ? "description-error" : undefined
-            }
-          />
-        </label>
-        {actionData?.errors?.description && (
-          <div className="pt-1 text-red-700" id="description-error">
-            {actionData.errors.description}
-          </div>
-        )}
-      </div>
+      <LabelledInput
+        rows={4}
+        label="Description"
+        ref={descriptionRef}
+        name="description"
+        error={actionData?.errors?.description}
+      />
 
       <div className="text-right">
-        <button
-          type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-        >
-          Save
-        </button>
+        <Button type="submit">Save</Button>
       </div>
     </Form>
   );
